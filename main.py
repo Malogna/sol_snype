a47a62d175d0f5d67192025fd0163acdb25dd785
Malogn <noahmarcelh@gmail.com>
2024-03-16 02:27:46 +0100

    fix stuff

--- main.py
+++ main.py
@@ -16,15 +16,46 @@ from soldexpy.wallet import Wallet
 
 from colorama import init
 
+import errno
+import os
+import signal
+import functools
+
 init(autoreset=True)
 
-print(text2art("yosharu-sol-snype"))
+
+class TimeoutError(Exception):
+    pass
+
+
+def timeout(seconds=10, error_message=os.strerror(errno.ETIME)):
+    def decorator(func):
+        def _handle_timeout(signum, frame):
+            raise TimeoutError(error_message)
+
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            signal.signal(signal.SIGALRM, _handle_timeout)
+            signal.alarm(seconds)
+            try:
+                result = func(*args, **kwargs)
+            finally:
+                signal.alarm(0)
+            return result
+
+        return wrapper
+
+    return decorator
+
 
 # load private key
 keypair = Keypair.from_bytes(base58.b58decode(PRIVATE_KEY))
 # configure rpc client
 client = Client("https://api.mainnet-beta.solana.com")
 sol_wal = Wallet(client, keypair.pubkey())
+
+print(text2art("yosharu-sol-snype"))
+print('SOL balance:', sol_wal.get_sol_balance(), 'SOL')
 
 # get pool
 while True:
@@ -60,8 +91,10 @@ pool = RaydiumPool(client, pool_address)
 swap = Swap(client, pool)
 print('Connected to Raydium!           ')
 
-print('SOL balance:', sol_wal.get_sol_balance(), 'SOL')
-print(f'${coin_symbol} balance: {sol_wal.get_balance(pool)[0]}')
+try:
+    print(f'${coin_symbol} balance: {sol_wal.get_balance(pool)[0]}')
+except TypeError:
+    pass
 
 if QUICK_BUY is False:
     ask_for_action = str(input("Action (b or s): "))
@@ -71,45 +104,44 @@ else:
     ask_for_action = 'b'
     ask_for_in_amount = QUICK_BUY_AMOUNT
 
+    # Read in the file
+    with open('config.py', 'r') as file:
+        filedata = file.read()
+
+    # Replace the target string
+    filedata = filedata.replace('QUICK_BUY = True', 'QUICK_BUY = False')
+
+    # Write the file out again
+    with open('config.py', 'w') as file:
+        file.write(filedata)
+
 print('Sending transaction...')
 time_start = time.time()
 
-
-class AbnornmalTransaction(Exception):
-    pass
-
-
 try:
-    while True:
-        try:
-            if ask_for_action == "b":
-                if ask_for_in_amount == "all":
-                    sol_wal_balance = sol_wal.get_sol_balance() * 0.95
-                    swap.buy(sol_wal_balance, SLIPPAGE, keypair)
-                else:
-                    swap.buy(float(ask_for_in_amount), SLIPPAGE, keypair)
+    if ask_for_action == "b":
+        if ask_for_in_amount == "all":
+            sol_wal_balance = sol_wal.get_sol_balance() * 0.95
+            swap_txn = swap.buy(sol_wal_balance, SLIPPAGE, keypair)
+        else:
+            swap_txn = swap.buy(float(ask_for_in_amount), SLIPPAGE, keypair)
 
-            if ask_for_action == "s":
-                if ask_for_in_amount == "all":
-                    token_wal_balance = sol_wal.get_balance(pool)[0]
-                    swap.sell(token_wal_balance, SLIPPAGE, keypair)
-                else:
-                    swap.sell(float(ask_for_in_amount), SLIPPAGE, keypair)
+    if ask_for_action == "s":
+        if ask_for_in_amount == "all":
+            token_wal_balance = sol_wal.get_balance(pool)[0]
+            swap_txn = swap.sell(token_wal_balance, SLIPPAGE, keypair)
+        else:
+            swap_txn = swap.sell(float(ask_for_in_amount), SLIPPAGE, keypair)
 
-            time_end = time.time()
-            time_spent = round(time_end - time_start, 2)
+    time_end = time.time()
+    time_spent = round(time_end - time_start, 2)
 
-            if time_spent < 0.1:
-                raise AbnornmalTransaction()
-
-            print(Fore.GREEN + 'Success!' + Fore.RESET + f' ({time_spent}s)')
-            break
-        except Exception:
-            print('Abnormal transaction, trying again...')
+    if 'status: Ok(())' in str(swap_txn):
+        print(Fore.GREEN + 'Success! Transaction confirmed.' + Fore.RESET + f' ({time_spent}s)')
 except Exception:
     type, value, traceback = sys.exc_info()
     if 'insufficient' in str(value):
-        print(Fore.RED + 'Transaction failed! Wallet has insufficient funds.')
+        print(Fore.RED + 'Transaction failed! Wallet has insufficient funds for this transaction.')
     else:
         print(Fore.RED + 'Transaction failed! Unknown error. Traceback:')
         print(value)
